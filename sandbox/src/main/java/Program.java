f78ca38fe57258e9714563d461ba1a5d37827f8d
a.karasenko <asya2794@mail.ru>
2018-10-18 23:03:59 +0700

    task 2

--- sandbox/src/main/java/Program.java
+++ sandbox/src/main/java/Program.java
@@ -1,7 +1,9 @@
 public class Program
 {
-    public static void main(String[] args)
-{
-System.out.println("Hello, world!");
-}
+    public static void main(String[] args) {
+        var p1 = new Point();
+        var p2 = new Point(1,2);
+
+        var r = Point.distance(p1, p2);
+    }
 }
