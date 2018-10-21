937cb55aa51deb6a7fecfd423626d7d701f620ea
a.karasenko <asya2794@mail.ru>
2018-10-21 22:07:35 +0700

    2.8

--- addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -10,7 +10,7 @@ public class GroupCreationTests  extends TestBase {
         app.getNavigationHelper().gotoGroupPage();
         app.getGroupHelper().initGroupCreation();
         app.getGroupHelper().fillGroupForm(new GroupData("testgroup", "mytestgroup", "testheader"));
-        app.submitGroupCreation();
+        app.getGroupHelper().submitGroupCreation();
         app.getGroupHelper().retutnToGroupPage();
     }
 
