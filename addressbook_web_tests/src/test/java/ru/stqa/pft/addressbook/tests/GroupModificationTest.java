330e32d58bee45f4bce56ccb6db8fd42d4b694d4
a.karasenko <asya2794@mail.ru>
2018-11-08 22:02:11 +0700

    4.10

--- addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
+++ addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
@@ -4,21 +4,22 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTest extends TestBase {
 
     @Test
-    public void testGroupModification(){
+    public void testGroupModification() {
         app.getNavigationHelper().gotoGroupPage();
-        if (!app.getGroupHelper().isThereAGroup()){
+        if (!app.getGroupHelper().isThereAGroup()) {
             app.getGroupHelper().addGroup(new GroupData("testgroup", "mytestgroup", "testheader"));
         }
 
         List<GroupData> before = app.getGroupHelper().getGroupList();
 
-        GroupData modefiedData = new GroupData( before.get(0).getId(), "newName", "newHeader", "newFooter");
+        GroupData modefiedData = new GroupData(before.get(0).getId(), "newName", "newHeader", "newFooter");
 
         app.getGroupHelper().selectGroup(0);
         app.getGroupHelper().ininGroupModification();
@@ -28,8 +29,14 @@ public class GroupModificationTest extends TestBase {
 
         List<GroupData> after = app.getGroupHelper().getGroupList();
 
+        Comparator<? super GroupData> byId = (Comparator<GroupData>) (o1, o2) -> Integer.compare(o1.getId(), o2.getId());
+        before.sort(byId);
+        after.sort(byId);
+
+        /*
         before.remove(0);
         before.add(modefiedData);
+        */
 
         Assert.assertEquals(new HashSet<GroupData>(before), new HashSet<GroupData>(after));
     }
